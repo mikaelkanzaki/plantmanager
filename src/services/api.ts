f52a915d34b79d145b89c53b827527cf924ee0a0
mikaelkanzaki <mikaelkanzaki@gmail.com>
2021-04-25 14:45:54 -0300

    Testando o my-json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.50.68:3333',
+  baseURL: 'https://my-json-server.typicode.com/mikaelkanzaki/plantmanager',
 });
 
 export default api;
